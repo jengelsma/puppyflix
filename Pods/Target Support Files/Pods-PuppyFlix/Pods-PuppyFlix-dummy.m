b0f808c9f2e379f8ab61a7812c343f16ce17dac5
Jonathan Engelsma <jengelsma@gmail.com>
2016-02-15 15:19:43 -0500

    Initial commit.

---
+++ Pods/Target Support Files/Pods-PuppyFlix/Pods-PuppyFlix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PuppyFlix : NSObject
+@end
+@implementation PodsDummy_Pods_PuppyFlix
+@end
---
+++ Pods/Target Support Files/Pods-PuppyFlixUITests/Pods-PuppyFlixUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PuppyFlixUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PuppyFlixUITestsVersionString[];
+
